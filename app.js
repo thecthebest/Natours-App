464ee4bea32dc8fea410d4ef279f6aa5af1364cd
thecthebest <you@example.com>
2021-08-30 11:37:58 +0100

    Added a route for sending all tours
    The route will listen to requests coming on "/api/v1/tours" with GET method and then respond to the requests by calling a callback route handler function which will send a status code with json.

--- app.js
+++ app.js
@@ -7,3 +7,13 @@ const fs = require('fs');
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/tours-simple.json`)
 );
+
+// Route for '/' url + method with route Handler function
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tours: tours
+        }
+    });
+});
