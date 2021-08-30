2e757df92fbeec9ebe5bdb940a3fa5ba4936cadc
thecthebest <you@example.com>
2021-08-30 11:36:49 +0100

    Added tours variable
    The tours variable will be used to store the tours .json data from "fs.readFileSync" & "JSON.parse".

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const express = require('express');
 const app = express();
 
 const fs = require('fs');
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/tours-simple.json`)
+);
