a184314c1bd66192da0707f0d1e4b0f8765c9921
thecthebest <you@example.com>
2021-09-05 11:45:03 +0100

    Added a functionality to store new requests
    When a post request arrives, the request handler function is called and two variables are created "newId" & "newTour". The "newId" is created by counting total numbers of elements and generating a new number. "newTour" is obtained by merging the object with the "newId" with the request object. Finally "newTour" is added with the exisiting elements and then values are saved in the json file and then a response is sent to the client containing the "newTour".

--- app.js
+++ app.js
@@ -22,7 +22,22 @@ app.get('/api/v1/tours', (req, res) => {
 
 // Route for '/' url + method with route Handler function
 app.post('/api/v1/tours', (req, res) => {
-
+    // Creating a new id number
+    const newId = tours[tours.length - 1].id + 1;
+    // creating a new object by mergining
+    const newTour = Object.assign({id: newId}, req.body);
+    // adding a new element
+    tours.push(newTour);
+    // Writing to the json file
+    fs.writeFile(`${__dirname}/dev-data/tours-simple.json`, JSON.stringify(tours), (err) => {
+        console.log("saved");
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tours: newTour
+            }
+        });
+    });
 });
 
 const port = 3000;
