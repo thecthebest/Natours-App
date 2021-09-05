a92d0f12008397466b0f81df37307e38b8c308ff
thecthebest <you@example.com>
2021-09-05 11:44:03 +0100

    Added a function to create tours
    The function listen to post requests and then calls a post handler function.

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ app.get('/api/v1/tours', (req, res) => {
     });
 });
 
+// Route for '/' url + method with route Handler function
+app.post('/api/v1/tours', (req, res) => {
+
+});
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
