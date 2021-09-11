cb25621143c035e92781075f3ba899ccd6ec0cee
thecthebest <you@example.com>
2021-09-11 11:47:45 +0100

    Added a functionality to update a tour
    When a patch request arrives, the request handler function is called and "id" variable is created. The "id" variable is checked against the numbers of items inside the "tours" and depending on the comparison either 404 or 200 status is returned.

--- app.js
+++ app.js
@@ -60,6 +60,23 @@ app.get('/api/v1/tours/:id', (req, res) => {
     });
 });
 
+// Route for '/:id' url + method with route Handler function
+app.patch('/api/v1/tours/:id', (req, res) => {
+    // Extract and convert to a number
+    const id = req.params.id * 1;
+    if (id > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '<Updated Tour...>'
+        }
+    });
+});
 
 const port = 3000;
 app.listen(port, () => {
