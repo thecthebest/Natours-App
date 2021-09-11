f634be23d832664812d7581988ddb279b05ad919
thecthebest <you@example.com>
2021-09-11 11:48:21 +0100

    Added a functionality to delete a tour
    When a delete request arrives, the request handler function is called and "id" variable is created. The "id" variable is checked against the numbers of items inside the "tours" and depending on the comparison either 404 or 200 status is returned.

--- app.js
+++ app.js
@@ -78,6 +78,22 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     });
 });
 
+// Route for '/:id' url + method with route Handler function
+app.delete('/api/v1/tours/:id', (req, res) => {
+    // Extract and convert to a number
+    const id = req.params.id * 1;
+    if (id > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    res.status(204).json({
+        status: 'success',
+        data: null
+    });
+});
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
