846053d2e93bc112819906dbdd851b13f6a01137
thecthebest <you@example.com>
2021-11-06 10:59:10 +0000

    Verified Express environment
    "app.get('env')" was used in the server.js file to verify Express environment. This code was added in the server.js file because anything not related to Express should be outside app.js(Environmental variable are outside the scope of express).

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const app = require('./app');
 
+// To check the current environment set by express
+console.log(app.get('env'));
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
