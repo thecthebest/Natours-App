095ce72c0f02d11e1830939b97d71421110a403d
thecthebest <you@example.com>
2021-11-06 11:11:00 +0000

    To see the environment variables set by Node.js
    This allow to see the environment variables set by Node.js and these are global variables that comes from process core modules, set when the process starts.

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = require('./app');
 // To check the current environment set by express
 console.log(app.get('env'));
 
+// To check the current environment variables set by nodeJS
+console.log(process.env);
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
